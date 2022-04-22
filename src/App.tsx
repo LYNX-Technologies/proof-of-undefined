be6f87fd338d666199735024fe7a5fe927b4bea5
Javier Bullrich <javier@bullrich.dev>
2022-04-22 22:07:56 +0200

    fixed ci not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,9 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Proof Of Undefined</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
