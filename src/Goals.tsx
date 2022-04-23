23adcbd52428b5fb8bcf8d6ca6d506215e1af499
AlexMcCarroll <alexandra@mccarrollhk.com>
2022-04-23 17:58:57 +0100

    modal added for goals

--- src/Goals.tsx
+++ src/Goals.tsx
@@ -1,15 +1,30 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import Grid from '@mui/material/Grid';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Modal, Box, Button, CardActionArea, CardActions, Card, Grid, CardContent, CardMedia, Typography, Slider} from '@mui/material';
 import MeditationImg from './assets/undraw_meditation.svg';
 import RunningImg from './assets/undraw_running.svg';
 import WalkingImg from './assets/undraw_walking.svg';
 
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+function valuetext(value: number) {
+    return `$DAI ${value}`;
+}
+
 export default function Goals() {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
   return (
     <div>
         <h1>Set Goals</h1>
@@ -37,7 +52,7 @@ export default function Goals() {
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary">
+                <Button size="small" color="primary" onClick={handleOpen}>
                     PLEDGE
                 </Button>
             </CardActions>
@@ -66,7 +81,7 @@ export default function Goals() {
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary">
+                <Button size="small" color="primary" onClick={handleOpen}>
                     PLEDGE
                 </Button>
             </CardActions>
@@ -95,12 +110,103 @@ export default function Goals() {
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary">
+                <Button size="small" color="primary" onClick={handleOpen}>
                     PLEDGE
                 </Button>
             </CardActions>
             </Card>
         </Grid>
+        <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Choose your pledge amount + length
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Amount($DAI)
+          </Typography>
+          <Box sx={{ width: 300 }}>
+            <Slider
+                aria-label=""
+                defaultValue={50}
+                getAriaValueText={valuetext}
+                step={5}
+                valueLabelDisplay="auto"
+                marks={marks}
+            />
+          </Box>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Length (weeks)
+          </Typography>
+          <Box sx={{ width: 300 }}>
+            <Slider
+                aria-label=""
+                defaultValue={4}
+                getAriaValueText={valuetext}
+                step={1}
+                valueLabelDisplay="auto"
+                marks={length}
+            />
+          </Box>
+          <Button size="small" color="primary" onClick={handleOpen}>
+            SUBMIT
+        </Button>
+        </Box>
+      </Modal>
     </div>
   );
 }
+
+const marks = [
+    {
+      value: 1,
+      label: '1',
+    },
+    {
+      value: 25,
+      label: '25',
+    },
+    {
+      value: 50,
+      label: '50',
+    },
+    {
+      value: 75,
+      label: '75',
+    },
+    {
+      value: 100,
+      label: '100',
+    },
+  ];
+
+  const length = [
+    {
+      value: 0,
+      label: '2',
+    },
+    {
+      value: 20,
+      label: '4',
+    },
+    {
+      value: 40,
+      label: '6',
+    },
+    {
+      value: 60,
+      label: '8',
+    },
+    {
+      value: 80,
+      label: '10',
+    },
+    {
+      value: 100,
+      label: '12',
+    }
+  ];
