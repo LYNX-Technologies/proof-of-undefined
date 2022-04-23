9109fceae3587e413c9cc126e06a9d996dbcd78e
AlexMcCarroll <alexandra@mccarrollhk.com>
2022-04-23 09:40:54 +0100

    error with webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import "./styles/App.css";
 import Oura from "./Oura";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Form from './components/form';
+import Modal from './components/modal';
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
@@ -8,6 +12,9 @@ function App() {
       <BrowserRouter>
         <div>
           <h2>NFTs yo</h2>
+          <Form />
+          <Button variant="contained">Connect Wallet</Button>
+          <Modal />
           <Routes>
             {/* <Route path="/" element={<Landing />} /> */}
             <Route path="/oura" element={<Oura />} />
