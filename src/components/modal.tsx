3aea2db6600d13b65ec7d48a505388b66139d216
Javier Bullrich <javier@bullrich.dev>
2022-04-23 11:25:32 +0200

    removed linting issue

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -4,6 +4,7 @@ import { useCallback, useEffect, useReducer } from 'react';
 import Web3Modal from 'web3modal';
 import { ellipseAddress, getChainData } from '../lib/utilities';
 
+// @ts-ignore
 const INFURA_ID = '460f40a260564ac4a4f4b3fffb032dad'
 
 const providerOptions = {
