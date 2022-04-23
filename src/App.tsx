480100704fa67bf18fe8d0471ada2ab05c5f5877
Oliver Klingefjord <oliver@kry.se>
2022-04-23 20:15:44 +0200

    placeholder dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,24 @@ import "./styles/App.css";
 import Goals from "./Goals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import OuraRedirectPage from "./OuraRedirect";
-import Login from './login';
+import Login from "./login";
 import { ServerContext } from "./contexts/wallet";
+import Dashboard from "./Dashboard";
 
 window.Buffer = window.Buffer || require("buffer").Buffer;
 
 function App() {
   return (
-    <ServerContext>
-      <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/Goals" element={<Goals />} />
-            <Route path="/oura_redirect" element={<OuraRedirectPage />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </ServerContext>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Goals" element={<Goals />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/oura_redirect" element={<OuraRedirectPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/Dashboard.tsx
@@ -0,0 +1,105 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import OuraImg from "./assets/oura.png";
+
+type Props = {
+  date: string;
+};
+
+const MeditationCard = (props: Props) => {
+  return (
+    <Card sx={{ maxWidth: 345, m: 1 }}>
+      <CardContent>
+        <img
+          src={OuraImg}
+          alt="Oura"
+          width={24}
+          height={24}
+          style={{
+            float: "right",
+          }}
+        />
+        <Typography gutterBottom variant="h5" component="div">
+          5 min meditation
+        </Typography>
+        <Typography variant="subtitle1" color="text.secondary">
+          {props.date}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+const MeditationColumn = () => {
+  return (
+    <Grid item xs={4}>
+      <Typography
+        variant="subtitle1"
+        color="text.secondary"
+        sx={{ paddingTop: "1rem" }}
+      >
+        Completed
+      </Typography>
+      <MeditationCard date={"Thursday 1:21 PM"} />
+      <MeditationCard date={"Friday 10:34 AM"} />
+    </Grid>
+  );
+};
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <Grid container spacing={2} alignItems="start">
+        <Grid item xs={8}>
+          <Card>
+            <Stack direction="column" justifyContent="start">
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ paddingTop: "1rem" }}
+              >
+                Goal
+              </Typography>
+              <Typography variant="h4">Meditation</Typography>
+
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ paddingTop: "1rem" }}
+              >
+                Time Left
+              </Typography>
+              <Typography variant="h4">2 days</Typography>
+
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ paddingTop: "1rem" }}
+              >
+                Progress
+              </Typography>
+              <Typography variant="h4">2 / 4</Typography>
+
+              <Typography
+                variant="subtitle1"
+                color="text.secondary"
+                sx={{ paddingTop: "1rem" }}
+              >
+                Money Staked
+              </Typography>
+              <Typography variant="h4">50 DAI</Typography>
+            </Stack>
+          </Card>
+        </Grid>
+        <MeditationColumn />
+      </Grid>
+    </div>
+  );
+}
